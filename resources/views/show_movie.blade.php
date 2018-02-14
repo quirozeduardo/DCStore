976506252eb2795db1e942d8efa90c963c05c20c
eduardo quiroz <quirozeduardo425@gmail.com>
2018-02-13 19:01:22 -0600

    13022018

--- resources/views/show_movie.blade.php
+++ resources/views/show_movie.blade.php
@@ -1,3 +1,4 @@
-<div>
-  <h2>{{$datasheet->original_title}}</h2>
+<div class="show-movie-container">
+  <h2 class="text-center" >{{$datasheet->original_title}}</h2>
+  
 </div>
---
+++ resources/views/templates/article_view.blade.php
@@ -0,0 +1,12 @@
+<div class="show-movie-container">
+  <h2 class="text-center" >{{$datasheet->original_title}}</h2>
+  <div>
+  	<img src="{{$img}}" alt="{{$datasheet->original_title}}">
+  </div>
+  <div>
+  	
+  </div>
+  <div>
+  	
+  </div>
+</div>
